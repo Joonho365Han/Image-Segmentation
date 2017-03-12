2a8d9326d5b934012eaadbfbcb259d820a7524aa
Joonho Han <joonho@protonmail.com>
2017-03-12 07:28:36 -0400

    The issue of fread() returning 0 has been fixed. Turns out that the pointer which I allocated the memory buffer to wasn't being passed betwen functions, so when I loaded the image on the memory, the pointer in the main function (img) was not pointing to the same location

--- Scalar/mrf.c
+++ Scalar/mrf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <unistd.h>
 
-#define TEMPERATURE 1
-#define THRESHOLD 0.99999999
-#define SECOND_ORDER
+#define TEMPERATURE 5
+#define ITERATIONS 20
+#define THRESHOLD 0.8
+// #define SECOND_ORDER
+// #define THIRD_ORDER
 
 #pragma pack(push, 1)
 typedef struct
@@ -33,19 +36,18 @@ typedef struct
     unsigned int blueChannelBitmask;
     unsigned int AlphaChannelBitmask;
     unsigned int ColorSpaceType;
-    int ColorSpaceEndpoints; //////////
+    int ColorSpaceEndpoints;
     unsigned int gammaChannelRed;
     unsigned int gammaChannelGreen;
     unsigned int gammaChannelBlue;
 } BMPINFOHEADER;
 #pragma pack(pop)
 
-int load_bitmap(char *filename, BMPINFOHEADER *bmpInfoHeader, unsigned char *img)
+int load_bitmap(char *filename, BMPINFOHEADER *bmpInfoHeader, unsigned char **img)
 {
     //  1. Open filename in read binary mode
     FILE *filePtr = fopen(filename,"rb");
-    if (filePtr == NULL)
-        return -1;
+    if (filePtr == NULL) return -1;
 
     //  2. Read the headers
     BMPFILEHEADER bmpFileHeader;
@@ -54,28 +56,24 @@ int load_bitmap(char *filename, BMPINFOHEADER *bmpInfoHeader, unsigned char *img
 
     //  3. malloc for bitmap
     int imgSize = bmpInfoHeader->biImageSize;
-    img = (unsigned char*) malloc(imgSize);
-    if (img == NULL)
-    {
-        free(img); fclose(filePtr); return -2;
-    }
-
-    printf("Bitmap offset: %d\n", bmpFileHeader.bfOffBits);
+    *img = (unsigned char*) malloc(imgSize*sizeof(unsigned char));
+    if (*img == NULL) return -2;
 
     //  4. Read the bitmap
     fseek(filePtr, bmpFileHeader.bfOffBits, SEEK_SET);
-    fread(img, imgSize, 1, filePtr);
+    int count = fread(*img, sizeof(unsigned char), imgSize, filePtr);
+    if (count == 0) printf("ERROR: Could not read anything\n");
+    if (count != imgSize) return count;
     fclose(filePtr);
 
     return 0;
 }
 
-int overwrite_bitmap(char *filename, unsigned char *img)
+int overwrite_bitmap(char *filename, unsigned char **img)
 {
     //  1. Open filename in "R/W binary at beginning" mode
     FILE *filePtr = fopen(filename, "rb+");
-    if (filePtr == NULL)
-        return -1;
+    if (filePtr == NULL) return -1;
 
     //  2. Read file header to find where and how much to write image
     BMPFILEHEADER bmpFileHeader;
@@ -85,7 +83,10 @@ int overwrite_bitmap(char *filename, unsigned char *img)
 
     //  3. Write all file info
     fseek(filePtr, bmpFileHeader.bfOffBits, SEEK_SET);
-    fwrite(img, bmpInfoHeader.biImageSize, 1, filePtr);
+    int imgSize = bmpInfoHeader.biImageSize;
+    int count = fwrite(*img, sizeof(unsigned char), imgSize, filePtr);
+    if (count == 0) printf("ERROR: Could not write anything\n");
+    if (count != imgSize) return count;
     fclose(filePtr);
 
     return 0;
@@ -97,81 +98,98 @@ int main(){
     char          *img_name = "image.bmp";
     BMPINFOHEADER  img_info;
     unsigned char *img;
-    switch (load_bitmap(img_name, &img_info, img))
-    {
-        case -1: printf("ERROR: File DNE\n");
-                 return 0;
-        case -2: printf("ERROR: Out of memory\n");
-                 return 0;
+    int status = load_bitmap(img_name, &img_info, &img);
+    if (status == -1) {
+        printf("ERROR: File DNE\n");
+        return 0;
+    } else if (status == -2){
+        printf("ERROR: Could not allocate memory\n");
+        return 0;
+    } else if (status != 0) {
+        printf("ERROR: Only read %d bytes\n", status);
+        return 0;
     }
-
-    printf("Imge info header size %d\n", img_info.biSize);
-    printf("Color planes %d\n", img_info.biPlanes);
-    printf("Bits per pixel %d\n", img_info.bibitPerPix);
-    printf("Compression rate: %d\n", img_info.biCompression);
-    printf("Image size: %d\n", img_info.biImageSize);
-    // printf("Red bitmask %x\n", img_info.redChannelBitmask);
-    // printf("Green bitmask %x\n", img_info.greenChannelBitmask);
-    // printf("Blue bitmask %x\n", img_info.blueChannelBitmask);
-
-    int g;
-    for (g = 0; g < img_info.biImageSize; g++)
-        if (img[g] != 0)
-            printf("%d\n", (int) img[g]);
 
     // 2. Relate image areas by thresholding PDF of Markovian Gibbs Probability
     //    (Refer to: Image Prediction)
-    unsigned char img_mask[img_info.biImageSize];
-    int i, j, k;
-    int byte_depth = img_info.bibitPerPix / 8;
-    int byte_padd  = (4 - img_info.biWidth * byte_depth % 4) % 4;
-    int byte_width = img_info.biWidth * byte_depth + byte_padd;
-    for (i = 1; i < img_info.biHeight-1; i++)
-        for (j = 1; j < img_info.biWidth-1; j++){
-            k = 0;
-                // Generate Gibbs CDF using Markovian Neighbors
-                double gibbs_CDF[257];      // Gibbs PDF for this pixel (i,j)'s
-                                            // luminance to be 0,1...255 or lower 
-                                            // based on Markovian neighbor values.
-                       gibbs_CDF[0] = 0;    // The first element is for making CDF 
-                                            // generation easier by having an index 0
-                                            // for lum -1, whose gibbs_PDF is 0.
-                int lum;
-                for (lum = 0; lum <= 255; lum++)
+    int h, i, j, k;
+    unsigned char *img_mask = (unsigned char*) malloc(img_info.biImageSize);
+    int byte_depth  = img_info.bibitPerPix / 8;
+    int byte_padd   = (4 - img_info.biWidth * byte_depth % 4) % 4;
+    int byte_width  = img_info.biWidth * byte_depth + byte_padd;
+    int byte_offset = 1;
+        #ifdef SECOND_ORDER
+        byte_offset = 2;
+        #endif
+        #ifdef THIRD_ORDER
+        byte_offset = 3;
+        #endif
+    for (h = 0; h < ITERATIONS; h++)
+        for (i = byte_offset; i < img_info.biHeight-byte_offset; i++)
+            for (j = byte_offset; j < img_info.biWidth-byte_offset; j++)
+                for (k = 0; k < byte_depth; k++)
                 {
-                    int Eq  = (img[(i-1)*byte_width + j*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
-                        Eq += (img[(i+1)*byte_width + j*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
-                        Eq += (img[i*byte_width + (j-1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
-                        Eq += (img[i*byte_width + (j+1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
-                        #ifdef SECOND_ORDER
-                        Eq += (img[(i-1)*byte_width + (j-1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
-                        Eq += (img[(i-1)*byte_width + (j+1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
-                        Eq += (img[(i+1)*byte_width + (j-1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
-                        Eq += (img[(i+1)*byte_width + (j+1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
-                        #endif
+                    // Generate Gibbs CDF using Markovian Neighbors
+                    double gibbs_CDF[257];      // Gibbs PDF for this pixel (i,j)'s
+                                                // luminance to be 0,1...255 or lower 
+                                                // based on Markovian neighbor values.
+                           gibbs_CDF[0] = 0;    // The first element is for making CDF 
+                                                // generation easier by having an index 0
+                                                // for lum -1, whose gibbs_PDF is 0.
+                    int lum;
+                    for (lum = 0; lum <= 255; lum++)
+                    {
+                        int Eq  = (img[(i-1)*byte_width + j*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i+1)*byte_width + j*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[i*byte_width + (j-1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[i*byte_width + (j+1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            #ifdef SECOND_ORDER
+                            Eq += (img[(i-1)*byte_width + (j-1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i-1)*byte_width + (j+1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i+1)*byte_width + (j-1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i+1)*byte_width + (j+1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i-2)*byte_width + j*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i+2)*byte_width + j*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[i*byte_width + (j-2)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[i*byte_width + (j+2)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            #endif
+                            #ifdef THIRD_ORDER
+                            Eq += (img[(i-2)*byte_width + (j+1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i-2)*byte_width + (j-1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i+2)*byte_width + (j+1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i+2)*byte_width + (j-1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i+1)*byte_width + (j-2)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i-1)*byte_width + (j-2)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i+1)*byte_width + (j+2)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i-1)*byte_width + (j+2)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i-3)*byte_width + j*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[(i+3)*byte_width + j*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[i*byte_width + (j-3)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            Eq += (img[i*byte_width + (j+3)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                            #endif
+                        gibbs_CDF[lum+1] = gibbs_CDF[lum] + exp(-Eq/TEMPERATURE);
+                    }
 
-                    // if (Eq < 10)
-                    //     printf("Eq less than 10 for (%d,%d). of value %d\n", i,j,lum);
-                    gibbs_CDF[lum+1] = gibbs_CDF[lum] + exp((double) (-Eq / TEMPERATURE));
+                    // Threshold CDF
+                    for (lum = 0; lum <= 255; lum++)
+                        if (gibbs_CDF[lum+1]/gibbs_CDF[256] > THRESHOLD)
+                        {
+                            img_mask[i*byte_width+j*byte_depth+k] = (unsigned char) lum;
+                            lum = 256;
+                        }
                 }
 
-                // Threshold CDF
-                for (lum = 0; lum <= 255; lum++)
-                    if (gibbs_CDF[lum+1]/gibbs_CDF[256] > THRESHOLD)
-                    {
-                        // printf("CDF is %6.8f, pixel (%d,%d,%d) is %d\n", gibbs_CDF[lum+1]/gibbs_CDF[256],i,j,k,lum);
-                        img_mask[i*byte_width+j*byte_depth+k] = (unsigned char) lum;
-                        lum = 256;
-                    }
-        }
-
     //  5. Save bitmap
-    if (overwrite_bitmap(img_name, img_mask))
-    {
-        printf("ERROR: File I/O error\n");
+    status = overwrite_bitmap(img_name, &img_mask);
+    if (status == -1) {
+        printf("ERROR: Could not open file\n");
+        return 0;
+    } else if (status != 0) {
+        printf("ERROR: Only wrote %d bytes\n", status);
         return 0;
     }
 
     free(img);
+    free(img_mask);
     return 0;
 }
