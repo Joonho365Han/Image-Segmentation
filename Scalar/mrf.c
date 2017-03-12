0d24629e2395a4c8d5fe9fb60a830ecf4d28c15e
Joonho Han <joonho@protonmail.com>
2017-03-11 21:04:05 -0500

    Finished the first version of code, but somehow does not read from the bitmap file; it reads 0s as the image. Must be file reading issue. I found this out by saving the modified image and seeing what it looks like. The funny thing is, when I read and save the image without any modification, the image is correct; it is not a black image. I must figure out what's going on

--- Scalar/mrf.c
+++ Scalar/mrf.c
@@ -3,7 +3,7 @@
 #include <math.h>
 
 #define TEMPERATURE 1
-#define THRESHOLD 0.7
+#define THRESHOLD 0.99999999
 #define SECOND_ORDER
 
 #pragma pack(push, 1)
@@ -15,22 +15,28 @@ typedef struct
     short int bfReserved2;  // Reserved; must be 0
     int bfOffBits;          // Specifies the offset in bytes from the bmpfileheader to the bmp bits
 } BMPFILEHEADER;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
 typedef struct
 {
-    int biSize;          // Specifies the number of bytes required by the struct
+    unsigned int biSize;          // Specifies the number of bytes required by the struct
     int biWidth;          // Specifies width in pixels
     int biHeight;         // Species height in pixels
-    short int biPlanes;         // Specifies the number of color planes, must be 1
-    short int biBitCount;       // Specifies the number of bit per pixel
-    int biCompression;   // Spcifies the type of compression
-    int biSizeImage;     // Size of image in bytes
+    unsigned short int biPlanes;         // Specifies the number of color planes, must be 1
+    unsigned short int bibitPerPix;       // Specifies the number of bit per pixel
+    unsigned int biCompression;   // Spcifies the type of compression
+    unsigned int biImageSize;     // Size of image in bytes
     int biXPelsPerMeter;  // Number of pixels per meter in x axis
     int biYPelsPerMeter;  // Number of pixels per meter in y axis
-    int biClrUsed;       // Number of colors used by the bmp
-    int biClrImportant;  // Number of colors that are important
+    unsigned int biClrUsed;       // Number of colors used by the bmp
+    unsigned int biClrImportant;  // Number of colors that are important
+    unsigned int redChannelBitmask;
+    unsigned int greenChannelBitmask;
+    unsigned int blueChannelBitmask;
+    unsigned int AlphaChannelBitmask;
+    unsigned int ColorSpaceType;
+    int ColorSpaceEndpoints; //////////
+    unsigned int gammaChannelRed;
+    unsigned int gammaChannelGreen;
+    unsigned int gammaChannelBlue;
 } BMPINFOHEADER;
 #pragma pack(pop)
 
@@ -47,12 +53,14 @@ int load_bitmap(char *filename, BMPINFOHEADER *bmpInfoHeader, unsigned char *img
     fread(bmpInfoHeader, sizeof(BMPINFOHEADER), 1, filePtr);
 
     //  3. malloc for bitmap
-    int imgSize = bmpInfoHeader->biSizeImage;
+    int imgSize = bmpInfoHeader->biImageSize;
     img = (unsigned char*) malloc(imgSize);
     if (img == NULL)
     {
         free(img); fclose(filePtr); return -2;
     }
+
+    printf("Bitmap offset: %d\n", bmpFileHeader.bfOffBits);
 
     //  4. Read the bitmap
     fseek(filePtr, bmpFileHeader.bfOffBits, SEEK_SET);
@@ -77,7 +85,7 @@ int overwrite_bitmap(char *filename, unsigned char *img)
 
     //  3. Write all file info
     fseek(filePtr, bmpFileHeader.bfOffBits, SEEK_SET);
-    fwrite(img, bmpInfoHeader.biSizeImage, 1, filePtr);
+    fwrite(img, bmpInfoHeader.biImageSize, 1, filePtr);
     fclose(filePtr);
 
     return 0;
@@ -97,16 +105,30 @@ int main(){
                  return 0;
     }
 
-    //  2. Relate image areas by thresholding PDF of Markovian Gibbs Probability
-    //     (Refer to: Image Prediction)
-    unsigned char img_mask[img_info.biSizeImage];
+    printf("Imge info header size %d\n", img_info.biSize);
+    printf("Color planes %d\n", img_info.biPlanes);
+    printf("Bits per pixel %d\n", img_info.bibitPerPix);
+    printf("Compression rate: %d\n", img_info.biCompression);
+    printf("Image size: %d\n", img_info.biImageSize);
+    // printf("Red bitmask %x\n", img_info.redChannelBitmask);
+    // printf("Green bitmask %x\n", img_info.greenChannelBitmask);
+    // printf("Blue bitmask %x\n", img_info.blueChannelBitmask);
+
+    int g;
+    for (g = 0; g < img_info.biImageSize; g++)
+        if (img[g] != 0)
+            printf("%d\n", (int) img[g]);
+
+    // 2. Relate image areas by thresholding PDF of Markovian Gibbs Probability
+    //    (Refer to: Image Prediction)
+    unsigned char img_mask[img_info.biImageSize];
     int i, j, k;
-    int byte_depth = img_info.biBitCount / 8;
-    int byte_width = img_info.biWidth * byte_depth;
-    for (i = 1; i < img_info.biHeight; i++)
-        for (j = 1; j < img_info.biWidth; j++)
-            for (k = 0; k < byte_depth; k++)
-            {
+    int byte_depth = img_info.bibitPerPix / 8;
+    int byte_padd  = (4 - img_info.biWidth * byte_depth % 4) % 4;
+    int byte_width = img_info.biWidth * byte_depth + byte_padd;
+    for (i = 1; i < img_info.biHeight-1; i++)
+        for (j = 1; j < img_info.biWidth-1; j++){
+            k = 0;
                 // Generate Gibbs CDF using Markovian Neighbors
                 double gibbs_CDF[257];      // Gibbs PDF for this pixel (i,j)'s
                                             // luminance to be 0,1...255 or lower 
@@ -117,17 +139,19 @@ int main(){
                 int lum;
                 for (lum = 0; lum <= 255; lum++)
                 {
-                    int Eq  = (img[(i-1)*byte_width + j*byte_depth + k] != lum) ? 5 : 0;
-                        Eq += (img[(i+1)*byte_width + j*byte_depth + k] != lum) ? 5 : 0;
-                        Eq += (img[i*byte_width + (j-1)*byte_depth + k] != lum) ? 5 : 0;
-                        Eq += (img[i*byte_width + (j+1)*byte_depth + k] != lum) ? 5 : 0;
+                    int Eq  = (img[(i-1)*byte_width + j*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                        Eq += (img[(i+1)*byte_width + j*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                        Eq += (img[i*byte_width + (j-1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                        Eq += (img[i*byte_width + (j+1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
                         #ifdef SECOND_ORDER
-                        Eq += (img[(i-1)*byte_width + (j-1)*byte_depth + k] != lum) ? 5 : 0;
-                        Eq += (img[(i-1)*byte_width + (j+1)*byte_depth + k] != lum) ? 5 : 0;
-                        Eq += (img[(i+1)*byte_width + (j-1)*byte_depth + k] != lum) ? 5 : 0;
-                        Eq += (img[(i+1)*byte_width + (j+1)*byte_depth + k] != lum) ? 5 : 0;
+                        Eq += (img[(i-1)*byte_width + (j-1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                        Eq += (img[(i-1)*byte_width + (j+1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                        Eq += (img[(i+1)*byte_width + (j-1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
+                        Eq += (img[(i+1)*byte_width + (j+1)*byte_depth + k] != (unsigned char) lum) ? 5 : 0;
                         #endif
 
+                    // if (Eq < 10)
+                    //     printf("Eq less than 10 for (%d,%d). of value %d\n", i,j,lum);
                     gibbs_CDF[lum+1] = gibbs_CDF[lum] + exp((double) (-Eq / TEMPERATURE));
                 }
 
@@ -135,11 +159,11 @@ int main(){
                 for (lum = 0; lum <= 255; lum++)
                     if (gibbs_CDF[lum+1]/gibbs_CDF[256] > THRESHOLD)
                     {
-                        printf("pixel (%d,%d,%d) is %d\n", i,j,k,lum);
+                        // printf("CDF is %6.8f, pixel (%d,%d,%d) is %d\n", gibbs_CDF[lum+1]/gibbs_CDF[256],i,j,k,lum);
                         img_mask[i*byte_width+j*byte_depth+k] = (unsigned char) lum;
                         lum = 256;
                     }
-            }
+        }
 
     //  5. Save bitmap
     if (overwrite_bitmap(img_name, img_mask))
